b72b2d2735416267f8a53a3ac9f099e720c650c6
brucebett <bett6448@gmail.com>
2024-07-15 09:50:07 +0300

    today mon update

--- Booksproject/settings.py
+++ Booksproject/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'Booksproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'Books Database',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
